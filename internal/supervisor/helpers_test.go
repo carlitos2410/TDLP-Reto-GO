ed77121f1a79c49f3585da12abb627ea929f55d7
CarlosBarriosSanchez <bsanchezcarlosd@uss.edu.pe>
2026-07-25 00:58:57 -0500

    Helpers_Carlos

---
+++ internal/supervisor/helpers_test.go
@@ -0,0 +1,17 @@
+package supervisor
+
+import "runtime"
+
+func testEchoCommand() (string, []string) {
+	if runtime.GOOS == "windows" {
+		return "cmd", []string{"/C", "echo", "hola"}
+	}
+	return "echo", []string{"hola"}
+}
+
+func testFailingCommand() (string, []string) {
+	if runtime.GOOS == "windows" {
+		return "cmd", []string{"/C", "exit", "1"}
+	}
+	return "sh", []string{"-c", "exit 1"}
+}
